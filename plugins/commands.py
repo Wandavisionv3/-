53d49e165a07a8cd6b6e35a9ac2a762394ea4823
Scarletwitch <101384551+Wandavisionv3@users.noreply.github.com>
2022-05-03 12:53:58 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -68,8 +68,9 @@ async def start(client, message):
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode='html'
-       )
-        if AUTH_CHANNEL and not await is_subscribed(client, message):
+        )
+        return
+    if AUTH_CHANNEL and not await is_subscribed(client, message):
         try:
             invite_link = await client.create_chat_invite_link(int(AUTH_CHANNEL))
         except ChatAdminRequired:
